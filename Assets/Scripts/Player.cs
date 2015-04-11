e7c9596c461cd09179421e9488418d2aae74cdef
Remi Papillie <remi.papillie@gmail.com>
2015-04-11 15:51:40 +0200

    basic block placement code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,12 +9,51 @@ public class Player : MonoBehaviour
     public float MouseSpeed = 1.0f;
     public float TargetSpeed = 10.0f;
     public Camera ChildCamera;
+    public GameObject SelectedBlock;
+    public float SelectionScale = 0.8f;
+    public float SelectionOffset = 0.2f;
     
     private Rigidbody m_rigidBody;
     
     void Start()
     {
+        Cursor.visible = false;
         m_rigidBody = GetComponent<Rigidbody>();
+    }
+    
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(ChildCamera.transform.position, ChildCamera.transform.forward, out hit))
+        {
+            if (!SelectedBlock)
+            {
+                // TODO: draw hovered block
+                
+                // try to grab a block
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    SelectedBlock = hit.collider.gameObject;
+                    SelectedBlock.transform.localScale = new Vector3(SelectionScale, SelectionScale, SelectionScale);
+                    hit.collider.enabled = false;
+                }
+            }
+            else
+            {
+                // move the selected block with the camera
+                var target = hit.collider.gameObject;
+                SelectedBlock.transform.position = target.transform.position + target.transform.up * SelectionOffset;
+                SelectedBlock.transform.rotation = target.transform.rotation;
+                
+                // try to stick a block to a wall
+                if (Input.GetButtonDown("Fire1"))
+                {
+                    SelectedBlock.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    SelectedBlock.GetComponent<Collider>().enabled = true;
+                    SelectedBlock = null;
+                }
+            }
+        }
     }
     
 	void FixedUpdate()
