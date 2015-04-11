6a52172f9ebe077d8b7ea7b72cb563c1f0e08679
Remi Papillie <remi.papillie@gmail.com>
2015-04-11 16:56:40 +0200

    placeable blocks

---
+++ Assets/Scripts/MoveableBlock.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveableBlock : MonoBehaviour
+{
+    // nothing here for now
+    // the component presence signals that the block can be moved around
+}
---
+++ Assets/Scripts/PlaceableBlock.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceableBlock : MonoBehaviour
+{
+    // nothing here for now
+    // the component presence signals that something can be put on top of it
+}
