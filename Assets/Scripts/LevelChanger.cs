4a0088fcf2343f4d5f3f95a74f6079014348fa64
Remi Papillie <remi.papillie@gmail.com>
2015-04-12 17:04:09 +0200

    niveau 1, level changer, build and stuff

---
+++ Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChanger : MonoBehaviour
+{
+    public string NextLevel;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        Application.LoadLevel(NextLevel);
+    }
+}
